43640f6b0bfc76b6d50c5bf60a0785aea92a88ef
Unrealized <kkpsykk@gmail.com>
2020-03-16 12:30:18 +0300

    + 2.10

--- chapter2/task10/main.cpp
+++ chapter2/task10/main.cpp
@@ -1,9 +1,15 @@
+#include <iomanip>
 #include <iostream>
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+int main() {
+  double F_old, S_old, P_old, F_new;
+  std::cout << "Enter the number of old pounds = ";
+  std::cin >> F_old;
+  std::cout << "Enter the number of old shillings = ";
+  std::cin >> S_old;
+  std::cout << "Enter the number of old pence = ";
+  std::cin >> P_old;
+  F_new = F_old + S_old / 20 + P_old / 240;
+  std::cout << "Convert to New Pounds = J" << std::setprecision(3) << F_new;
+  return 0;
 }
