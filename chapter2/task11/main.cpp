396bee909b1390247d159c13c036f6e9946ab641
Unrealized <kkpsykk@gmail.com>
2020-03-16 15:50:36 +0300

    2 chapter finished

--- chapter2/task11/main.cpp
+++ chapter2/task11/main.cpp
@@ -1,9 +1,20 @@
+#include <iomanip>
 #include <iostream>
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+int main() {
+  std::cout << std::setiosflags(std::ios::left) << std::setw(13) << "surname"
+            << std::setw(12) << "name" << std::setw(16) << "addres"
+            << std::setw(18) << "town" << std::endl;
+  std::cout << std::setw(57) << std::setfill('-') << "-" << std::setfill(' ')
+            << std::endl;
+  std::cout << std::setw(13) << "Petrov" << std::setw(12) << "Vasya"
+            << std::setw(16) << "klenoavaya 16" << std::setw(18)
+            << "Saint-Petersburg" << std::endl;
+  std::cout << std::setw(13) << "Ivanov" << std::setw(12) << "Serega"
+            << std::setw(16) << "Osinovaya 3" << std::setw(18) << "Nahodka"
+            << std::endl;
+  std::cout << std::setw(13) << "Sidorov" << std::setw(12) << "Vanya"
+            << std::setw(16) << "Berezovaya 21" << std::setw(18)
+            << "Kaliningrad" << std::endl;
+  return 0;
 }
--- chapter2/task12/main.cpp
+++ chapter2/task12/main.cpp
@@ -1,11 +1,20 @@
+#include <iomanip>
 #include <iostream>
 
-int main()
+int main() {
+  double F_new, s, p;
+  int F_old, S_old, P_old;
+  std::cout << "Enter the number of pounds = ";
+  std::cin >> F_new;
 
-{
-  double gal;
-  std::cout << "vvedite galon \n";
-  std::cin >> gal;
-  std::cout << "obem v futah = " << gal / 7.481;
+  F_old = static_cast<int>(F_new);
+  s = (F_new - F_old) * 20;
+  S_old = static_cast<int>(s);
+  p = (s - S_old) * 12;
+  P_old = static_cast<int>(p);
+
+  std::cout << "Convert to New Pounds = J" << F_old << "." << S_old << "."
+            << P_old;
+
   return 0;
 }
