0500341b47e3c49c4f6fc58ffd338b77192717fd
Unrealized <kkpsykk@gmail.com>
2020-03-24 11:32:13 +0300

    3.1 +

---
+++ chapter3/task1/main.cpp
@@ -0,0 +1,20 @@
+#include <iomanip>
+#include <iostream>
+
+int main()
+
+{
+  int x, y;
+  std::cout << "enter number";
+  std::cin >> x;
+  for (int j = 1; j <= 200; j++) {
+    y = x * j;
+    if (j % 10 > 0)
+      std::cout << std::setiosflags(std::ios::left) << std::setw(6) << y;
+    else
+
+      std::cout << std::setiosflags(std::ios::left) << std::setw(6) << y
+                << std::endl;
+  }
+  return 0;
+}
